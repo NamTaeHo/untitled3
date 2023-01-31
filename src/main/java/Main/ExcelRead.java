9a8cdefff91aae5cda5c75629ca41750cab51581
NamTaeHo <han127kr@gmail.com>
2023-01-31 10:00:48 +0900

    initial upload

---
+++ src/main/java/Main/ExcelRead.java
@@ -0,0 +1,15 @@
+package Main;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExcelRead {
+
+
+    FileInputStream A = new FileInputStream("C/Users/USER/Desktop/Log.xlsm");
+
+
+
+    public ExcelRead() throws FileNotFoundException {
+    }
+}
